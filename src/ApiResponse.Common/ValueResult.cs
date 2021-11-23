6b365e9845485675c5a4f659669970ac3febf5b6
Dmitry Grigorev <dmitryandreevichgr@gmail.com>
2021-11-23 14:53:26 +0300

    Added value result class

---
+++ src/ApiResponse.Common/ValueResult.cs
@@ -0,0 +1,38 @@
+﻿using JetBrains.Annotations;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ApiResponse.Common
+{
+    public class ValueResult<T> : Result
+    {
+        /// <summary>
+        /// Result value
+        /// </summary>
+        public T? Value { get; set; }
+
+        public static ValueResult<T> Ok([ItemCanBeNull] T? value) => new ValueResult<T>
+        {
+            IsSuccess = true,
+            Value = value
+        };
+
+        [NotNull]
+        public static new ValueResult<T> Fail([ItemNotNull] Error error) => new ValueResult<T> { IsSuccess = false, Error = error };
+
+        public static ValueResult<T> Fail(int errorCode, [ItemNotNull] string message)
+        {
+            if(StringUtils.StringIsNullOrWhitespace(message))
+                throw new ArgumentNullException(nameof(message));
+
+            return new ValueResult<T>
+            {
+                IsSuccess = false,
+                Error = new Error(errorCode, message)
+            };
+        }
+    }
+}
---
+++ test/Tests.ApiResponse.Common/ValueResultTests.cs
@@ -0,0 +1,38 @@
+﻿using ApiResponse.Common;
+using Moq;
+using NUnit.Framework;
+using System;
+
+namespace Tests.ApiResponse.Common
+{
+    [TestFixture]
+    public class ValueResultTests
+    {
+        class SampleClass
+        {
+        }
+
+        [SetUp]
+        public void SetUp()
+        {
+        }
+
+        [Test]
+        public void CreateOkValueResultInstance()
+        {
+            Assert.IsTrue(ValueResult<SampleClass>.Ok(default).IsSuccess);
+        }
+
+        [Test]
+        public void CreateFailResultInstance()
+        {
+            Assert.IsFalse(ValueResult<SampleClass>.Fail().IsSuccess);
+        }
+
+        [Test]
+        public void CreateFailResultWithNullErrorMessage()
+        {
+            Assert.Throws<ArgumentNullException>(() => ValueResult<SampleClass>.Fail(ErrorCodes.InternalError, null));
+        }
+    }
+}

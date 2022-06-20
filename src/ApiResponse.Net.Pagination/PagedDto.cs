f12ebad3603be648abf489383a23b24e0f23d1f6
Dmitry Grigorev <dmitryandreevichgr@gmail.com>
2022-06-20 15:57:57 +0300

    Added pagination model

---
+++ src/ApiResponse.Net.Pagination/PagedDto.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ApiResponse.Net.Pagination
+{
+    public class PagedDto<T>
+    {
+        public int PageIndex { get; set; }
+        public int PageSize { get; set; }
+        public int PagesCount { get; set; }
+        public IQueryable<T> Values { get; set; }
+        public bool HasMore { get; set; }
+    }
+}
---
+++ src/ApiResponse.Net.Pagination/PaginationModel.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Linq;
+
+namespace ApiResponse.Net.Pagination
+{
+    internal class PaginationModel<T>
+    {
+        private readonly int _pageSize;
+        private readonly int _pageIndex;
+        private readonly int _totalPages;
+        private readonly IQueryable<T> _sourceQuery;
+
+        public PaginationModel(IQueryable<T> queryResult, int pageSize, int startPageIndex)
+        {
+            this._pageSize = pageSize;
+            this._pageIndex = startPageIndex;
+            this._sourceQuery = queryResult;
+            this._totalPages = (int)(queryResult.Count() / pageSize);
+        }
+
+        public PagedDto<T> GetNextPage(int pageNum)
+        {
+            var skippableItemsCount = pageNum * _pageSize;
+            return _sourceQuery.Count() < skippableItemsCount ? 
+                _sourceQuery.ToPages(this._pageIndex, this._totalPages, this._pageSize) : 
+                _sourceQuery.Skip(pageNum * _pageSize).Take(_pageSize).ToPages(pageNum, this._totalPages, _pageSize);
+        }
+    }
+}

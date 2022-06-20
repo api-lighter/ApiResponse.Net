000c1e92076210bf928788a97947e60a977b8b91
Dmitry Grigorev <dmitryandreevichgr@gmail.com>
2022-06-20 18:37:11 +0300

    Fixed pagination logic. Added chain based instance passing.

--- src/ApiResponse.Net.Pagination/Extensions.cs
+++ src/ApiResponse.Net.Pagination/Extensions.cs
@@ -11,7 +11,19 @@ namespace ApiResponse.Net.Pagination
         public static PagedDto<T> ToPages<T>(this IQueryable<T> queryResult, int pageNumber, int pagesCount, int pageSize = 10)
         {
             var model = new PaginationModel<T>(queryResult, pageSize, pageNumber);
-            return model.GetNextPage(pageNumber);
+            return model.GetNextPage(pageNumber).ToPagedDto();
+        }
+
+        internal static PagedDto<T> ToPagedDto<T>(this PaginationModel<T> pagination)
+        {
+            return new PagedDto<T>
+            {
+                HasMore = pagination.HasMore,
+                PageIndex = pagination.PageIndex,
+                PagesCount = pagination.TotalPages,
+                PageSize = pagination.PageSize,
+                Values = pagination.QueryResult
+            };
         }
     }
 }
--- src/ApiResponse.Net.Pagination/PaginationModel.cs
+++ src/ApiResponse.Net.Pagination/PaginationModel.cs
@@ -12,18 +12,47 @@ namespace ApiResponse.Net.Pagination
 
         public PaginationModel(IQueryable<T> queryResult, int pageSize, int startPageIndex)
         {
+            if (pageSize <= 0)
+                throw new ArgumentException();
+            if (startPageIndex < 0)
+                throw new ArgumentException();
+            if (queryResult == null)
+                throw new ArgumentNullException(nameof(queryResult));
+
             this._pageSize = pageSize;
             this._pageIndex = startPageIndex;
             this._sourceQuery = queryResult;
             this._totalPages = (int)(queryResult.Count() / pageSize);
         }
 
-        public PagedDto<T> GetNextPage(int pageNum)
+        private PaginationModel(IQueryable<T> queryResult, int pageSize, int startPageIndex, int totalPages)
+        {
+            if (pageSize <= 0)
+                throw new ArgumentException();
+            if (startPageIndex < 0)
+                throw new ArgumentException();
+            if (queryResult == null)
+                throw new ArgumentNullException(nameof(queryResult));
+
+            this._pageSize = pageSize;
+            this._pageIndex = startPageIndex;
+            this._sourceQuery = queryResult;
+            this._totalPages = totalPages;
+        }
+
+        internal int PageSize => _pageSize;
+        internal int PageIndex => _pageIndex;
+        internal int TotalPages => _totalPages;
+        internal bool HasMore => _pageSize * _pageIndex < _sourceQuery.Count();
+        internal IQueryable<T> QueryResult => _sourceQuery;
+
+        internal PaginationModel<T> GetNextPage(int pageNum)
         {
             var skippableItemsCount = pageNum * _pageSize;
-            return _sourceQuery.Count() < skippableItemsCount ? 
-                _sourceQuery.ToPages(this._pageIndex, this._totalPages, this._pageSize) : 
-                _sourceQuery.Skip(pageNum * _pageSize).Take(_pageSize).ToPages(pageNum, this._totalPages, _pageSize);
+            var newModel = new PaginationModel<T>(_sourceQuery.Count() < skippableItemsCount ?
+                _sourceQuery :
+                _sourceQuery.Skip(pageNum * _pageSize).Take(_pageSize), _pageSize, pageNum, TotalPages);
+            return newModel;
         }
     }
 }
